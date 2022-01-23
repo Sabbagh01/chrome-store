3896c413f81376fd116ba95cebdd2a1718f4423a
Sabbagh01 <ma00017@mix.wvu.edu>
2022-01-23 16:35:17 -0500

    navbar 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TopNavBarComponent } from 'src/app/Navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { SecondTopNavBarComponent } from './Navigation/second.topnavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    SecondTopNavBarComponent
   ],
   imports: [
     BrowserModule
